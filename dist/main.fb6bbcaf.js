17f11662a3a7b4acf22d0f99e1606ecbbfe86be1
Clive Mangerere <cmangerere@gmail.com>
2023-08-24 14:54:38 -0400

    Made landing page responsive

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -144,7 +144,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58405" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51956" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
