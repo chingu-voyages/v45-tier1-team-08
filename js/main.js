973b58a29494ead03d04933d223e532991c14b54
Emimint <90863470+Emimint@users.noreply.github.com>
2023-09-06 15:18:09 -0400

    convert main.js code into makeTable function
    We can now export the code for displaying the table as function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-import { data as dataFile } from "/Team_Docs/testData.js";
-
+export function makeTable(tab) {
 //select table elemnt in the DOM
 const table = document.getElementById("showData");
 
 //creat array of <tr> elements for table
-const rows = dataFile.map((data) => {
+const rows = tab.map((data) => {
   return `<tr>
     <td>${data.name}</td>
     <td>${data.year}</td>
@@ -13,36 +12,37 @@ const rows = dataFile.map((data) => {
   </tr>`;
 });
 
-// insert array of <tr> elements into table
-table.querySelector("tbody").innerHTML = rows.join("");
+  // insert array of <tr> elements into table
+  table.querySelector("tbody").innerHTML = rows.join("");
 
-$(document).ready(function () {
-  // wait for page to load
-  $("th").click(function () {
-    // sorts table by clicking on header element
-    var table = $(this).parents("table").eq(0);
-    var rows = table
-      .find("tr:gt(0)")
-      .toArray()
-      .sort(comparer($(this).index()));
-    this.asc = !this.asc;
-    if (!this.asc) {
-      rows = rows.reverse();
+  $(document).ready(function () {
+    // wait for page to load
+    $("th").click(function () {
+      // sorts table by clicking on header element
+      var table = $(this).parents("table").eq(0);
+      var rows = table
+        .find("tr:gt(0)")
+        .toArray()
+        .sort(comparer($(this).index()));
+      this.asc = !this.asc;
+      if (!this.asc) {
+        rows = rows.reverse();
+      }
+      for (var i = 0; i < rows.length; i++) {
+        table.append(rows[i]);
+      }
+    });
+    function comparer(index) {
+      return function (a, b) {
+        var valA = getCellValue(a, index),
+          valB = getCellValue(b, index);
+        return $.isNumeric(valA) && $.isNumeric(valB)
+          ? valA - valB
+          : valA.toString().localeCompare(valB);
+      };
     }
-    for (var i = 0; i < rows.length; i++) {
-      table.append(rows[i]);
+    function getCellValue(row, index) {
+      return $(row).children("td").eq(index).text();
     }
   });
-  function comparer(index) {
-    return function (a, b) {
-      var valA = getCellValue(a, index),
-        valB = getCellValue(b, index);
-      return $.isNumeric(valA) && $.isNumeric(valB)
-        ? valA - valB
-        : valA.toString().localeCompare(valB);
-    };
-  }
-  function getCellValue(row, index) {
-    return $(row).children("td").eq(index).text();
-  }
-});
+}
