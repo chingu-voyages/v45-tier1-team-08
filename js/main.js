968ca16ccadcc911ca2ef13d27adbd6150aebba7
MrBarker47 <jebarker7@gmail.com>
2023-09-09 11:52:49 -0400

    added a new table

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 export function makeTable(tab) {
   //select table elemnt in the DOM
-  const table = document.getElementById("showData");
+  let table = document.getElementById("showData");
 
   //creat array of <tr> elements for table
   const rows = tab.map((data) => {
@@ -14,35 +14,36 @@ export function makeTable(tab) {
 
   // insert array of <tr> elements into table
   table.querySelector("tbody").innerHTML = rows.join("");
-
-  $(document).ready(function () {
-    // wait for page to load
-    $("th").click(function () {
-      // sorts table by clicking on header element
-      var table = $(this).parents("table").eq(0);
-      var rows = table
-        .find("tr:gt(0)")
-        .toArray()
-        .sort(comparer($(this).index()));
-      this.asc = !this.asc;
-      if (!this.asc) {
-        rows = rows.reverse();
-      }
-      for (var i = 0; i < rows.length; i++) {
-        table.append(rows[i]);
-      }
-    });
-    function comparer(index) {
-      return function (a, b) {
-        var valA = getCellValue(a, index),
-          valB = getCellValue(b, index);
-        return $.isNumeric(valA) && $.isNumeric(valB)
-          ? valA - valB
-          : valA.toString().localeCompare(valB);
-      };
-    }
-    function getCellValue(row, index) {
-      return $(row).children("td").eq(index).text();
-    }
-  });
+  console.log(table);
+  table = new DataTable('#showData');
+  // $(document).ready(function () {
+  //   // wait for page to load
+  //   $("th").click(function () {
+  //     // sorts table by clicking on header element
+  //     var table = $(this).parents("table").eq(0);
+  //     var rows = table
+  //       .find("tr:gt(0)")
+  //       .toArray()
+  //       .sort(comparer($(this).index()));
+  //     this.asc = !this.asc;
+  //     if (!this.asc) {
+  //       rows = rows.reverse();
+  //     }
+  //     for (var i = 0; i < rows.length; i++) {
+  //       table.append(rows[i]);
+  //     }
+  //   });
+  //   function comparer(index) {
+  //     return function (a, b) {
+  //       var valA = getCellValue(a, index),
+  //         valB = getCellValue(b, index);
+  //       return $.isNumeric(valA) && $.isNumeric(valB)
+  //         ? valA - valB
+  //         : valA.toString().localeCompare(valB);
+  //     };
+  //   }
+  //   function getCellValue(row, index) {
+  //     return $(row).children("td").eq(index).text();
+  //   }
+  // });
 }
