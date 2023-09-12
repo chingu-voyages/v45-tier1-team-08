696ff06fa90467751f1585c77e951f54386d475c
Emimint <90863470+Emimint@users.noreply.github.com>
2023-09-12 14:54:19 -0400

    fix bug in table display

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 let table = document.getElementById("showData");
 
 export function makeTable(tab) {
-  const rows = [
-    {
-      Name: "data.name",
-      Year: "data.year",
-      Composition: "data.recclass",
-      Mass: "data.mass_g",
-    }, // dummy data; should be a list of objects with exact same format. Have a look here: https://datatables.net/reference/option/data
-  ];
+  const rows = tab.map((data) => ({
+    Name: data.name,
+    Year: data.year,
+    Composition: data.recclass,
+    Mass: data.mass_g,
+  }));
 
   $("#showData").dataTable({
     data: rows,
@@ -22,18 +20,19 @@ export function makeTable(tab) {
   showData_filter.remove();
 }
 
-export function updateTable( tab ) {
+export function updateTable(tab) {
+  const rows = tab.map((data) => ({
+    Name: data.name,
+    Year: data.year,
+    Composition: data.recclass,
+    Mass: data.mass_g,
+  } ) );
+  
   // creation of a brand new table at each search call:
   table = new DataTable("#showData");
   table.clear().draw(); // will reset the current table
-  table.row
-    .add({
-      Name: "data.!!!!!",
-      Year: "data.!!!!!",
-      Composition: "data.!!!!!",
-      Mass: "data.!!!!!",
-    }) // same format here as well
+
+  table.rows
+    .add(rows)
     .draw();
 }
-
-// DataTables warning: table id=showData - Requested unknown parameter '0' for row 0, column 0. For more information about this error, please see http://datatables.net/tn/4
